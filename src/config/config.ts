ffce2175fffbfb0a8a37e6b79777938010cdf42c
Eli José Carrasquero <ielijose@gmail.com>
2023-06-23 15:04:24 -0600

    NFT Transfer

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,3 +4,7 @@ export const MINT_CONFIG = {
   TRIGGER_ID: process.env.NEXT_PUBLIC_NORAMP_MINT_TRIGGER_ID,
   APP_ID: process.env.NEXT_PUBLIC_NORAMP_MINT_APP_ID,
 };
+
+export const TRANSFER_CONFIG = {
+  PRICE_ID: process.env.NEXT_PUBLIC_NORAMP_TRANSFER_PRICE_ID,
+};
---
+++ src/pages/transfer.tsx
@@ -0,0 +1,98 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { NextPage } from 'next';
+import Image from 'next/image';
+import React, { useCallback, useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import Celebrate from '../components/Celebrate';
+import { TRANSFER_CONFIG } from '../config/config';
+
+const TransferPage: NextPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('message', (event) => {
+      if (event.data.event === 'noramp:onPayment') {
+        console.log('Payment event', event.data);
+
+        const type = event.data?.detail?.type;
+        const status = event.data?.detail?.data?.status;
+
+        if (type === 'finished' && status === 'paid') {
+          setSuccess(true);
+        }
+      }
+    });
+  }, []);
+
+  const handleSubmit = useCallback(
+    async (event: React.FormEvent<HTMLFormElement>) => {
+      setIsLoading(true);
+      event.preventDefault();
+
+      try {
+        // @ts-ignore
+        window.initializeNoRamp({
+          priceId: TRANSFER_CONFIG.PRICE_ID,
+          testnet: true,
+
+          onSuccess: async (data: any) => {
+            console.log('success', data);
+            // setSuccess(true);
+            setIsLoading(false);
+          },
+          onFailure: (err: any) => {
+            console.error(err);
+            if (err?.message) {
+              alert(err?.message);
+            }
+          },
+          onClose: (data: any) => {
+            console.log('closed', data);
+            setIsLoading(false);
+            setSuccess(false);
+          },
+          onEvent: console.log,
+        });
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        window.NoRamp.open();
+      } catch (e) {
+        toast.error('Error creating price');
+      }
+    },
+    []
+  );
+
+  return (
+    <div className="container flex flex-col items-center justify-center flex-1 gap-8 px-4 mx-auto">
+      <p className="text-2xl">Transfer NFT</p>
+
+      <Image
+        src="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png"
+        alt="USDC logo"
+        width={100}
+        height={100}
+      />
+
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center justify-center gap-8 m-0 leading-tight"
+      >
+        <button
+          className="w-full p-2.5 text-white bg-purple-500 rounded-[6px] hover:bg-opacity-80 transition-all duration-200 ease-in-out disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-opacity-50 "
+          type="submit"
+          disabled={isLoading}
+        >
+          {isLoading ? 'Loading...' : 'Transfer'}
+        </button>
+      </form>
+
+      {success && <Celebrate />}
+    </div>
+  );
+};
+
+export default TransferPage;
